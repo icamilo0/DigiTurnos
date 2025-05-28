63240d73b39209afb761bed90699d7b9f70f832c
Camilo Mercado <mercadoarizacamilo@gmail.com>
2025-05-28 17:11:59 -0500

    Update

--- digiturnos/esp32/digiturnos.ino
+++ digiturnos/esp32/digiturnos.ino
@@ -10,8 +10,8 @@ const char* password = "redmin13";
 // Pines de botones
 const int Ase1_Gn = 18; // Asesor 1 - General (P1)
 const int Ase1_Cn = 2;  // Asesor 1 - Consignaciones (P2)
-const int Ase2_Gn = 4;  // Asesor 2 - General (P3)
-const int Ase2_Cn = 5;  // Asesor 2 - Consignaciones (P4)
+const int Ase2_Gn = 14;  // Asesor 2 - General (P3)
+const int Ase2_Cn = 12;  // Asesor 2 - Consignaciones (P4)
 
 // Contadores únicos
 int contadorGeneral = 0;
@@ -43,7 +43,7 @@ void checkResetButton(bool pressed, int index, unsigned long now);
 String formatTurno(String tipo, String asesor, int num);
 
 // Variables para testeo automático
-bool modoTest = true; // Cambia a false para volver al modo normal
+bool modoTest = false; // Cambia a false para volver al modo normal
 unsigned long ultimoEnvioTest = 0;
 int turnoTest = 1;
 
@@ -58,16 +58,16 @@ void setup() {
 
   // Asociar cada pin al identificador de rebote - 50 ms ignorando lecturas del pulsado
     debouncerP1.attach(Ase1_Gn);
-    debouncerP1.interval(50);
+    debouncerP1.interval(100);
 
     debouncerP2.attach(Ase1_Cn);
-    debouncerP2.interval(50);
+    debouncerP2.interval(100);
 
     debouncerP3.attach(Ase2_Gn);
-    debouncerP3.interval(50);
+    debouncerP3.interval(100);
 
     debouncerP4.attach(Ase2_Cn);
-    debouncerP4.interval(50);
+    debouncerP4.interval(100);
 
     // Iniciar conexión WiFi
     setupWiFi();
@@ -123,6 +123,7 @@ void loop() {
 
     if (pressedCount > 1) {
         enviarTurno("ERROR");
+        Serial.print("ERROR");
         return;
     }
 
@@ -135,22 +136,29 @@ void loop() {
     // Evitar emitir turnos seguidos sin esperar 5s
     if (now - lastTurnTime < delayBetweenTurns) return;
 
+    // ...en tu loop, antes de la lógica de turnos
+    Serial.printf("P1:%d P2:%d P3:%d P4:%d\n", p1, p2, p3, p4);
+
     // Emitir turno según botón
     if (p1) {
         enviarTurno(formatTurno("Gn - A1", "Asesor 1", contadorGeneral));
         contadorGeneral = (contadorGeneral % limiteGeneral) + 1;
+        Serial.print("Gn - A1 | Asesor 1 | Turno: " + contadorGeneral);
         lastTurnTime = now;
     } else if (p3) {
         enviarTurno(formatTurno("Gn - A2", "Asesor 2", contadorGeneral));
         contadorGeneral = (contadorGeneral % limiteGeneral) + 1;
+        Serial.print("Gn - A2 | Asesor 2 | Turno: " + contadorGeneral);
         lastTurnTime = now;
     } else if (p2) {
         enviarTurno(formatTurno("Cn", "Asesor 1", contadorConsignacion));
         contadorConsignacion = (contadorConsignacion % limiteConsignacion) + 1;
+        Serial.print("Cn | Asesor 1 | Turno: " + contadorConsignacion);
         lastTurnTime = now;
     } else if (p4) {
         enviarTurno(formatTurno("Cn", "Asesor 2", contadorConsignacion));
         contadorConsignacion = (contadorConsignacion % limiteConsignacion) + 1;
+        Serial.print("Cn | Asesor 2 | Turno: " + contadorConsignacion);
         lastTurnTime = now;
     }
 }
@@ -175,6 +183,7 @@ void checkResetButton(bool pressed, int index, unsigned long now) {
             contadorGeneral = 0;
             contadorConsignacion = 0;
             enviarTurno("RESET");
+            Serial.print("RESET");
             buttonPressStartTime[index] = 0;
         }
     } else {
